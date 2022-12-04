843ca2d13bd33c0a80c2eac76b0ecfaabac5bb21
mnetke.dev <mnetke.dev@DESKTOP-0BP7HQF>
2022-12-04 12:33:14 +0530

    [Satish]:Two classes are created

---
+++ src/sample/samp1.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class samp1 
+{
+public static void main(String[] args) 
+{
+System.out.println("Get Repository");	
+}
+}
---
+++ src/sample/samp2.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class samp2
+{
+	public static void main(String[] args) 
+	{
+	System.out.println("Get Token");	
+	}
+	
+
+}
